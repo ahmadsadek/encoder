2777478e37d8ccbad9ad358015f26f470f7e1ef6
ahmadsadek <32934412+ahmadsadek@users.noreply.github.com>
2021-12-24 11:27:17 +0100

    First reasonable output
    - successfully encode the given structure
    - Next step the implementation will be moved inside the function API

--- encoder.go
+++ encoder.go
@@ -3,35 +3,42 @@ package main
 import (
 	"bytes"
 	"fmt"
-
 	"github.com/HewlettPackard/structex"
 )
 
+
 type Nas5GSUpdateType struct {
-	IEI           uint8                 // Octet 1
-	Length        uint8                 // Octet 2
-	EPS_PNB_CIoT  uint8 `bitfield:"1"`  // Octet 3
-	_5GS_PNB_CIoT uint8 `bitfield:"1"`
-	NG_RAN_RCU    uint8 `bitfield:"2"`
-	SMS_requested uint8 `bitfield:"2"`
-	spare0        uint8 `bitfield:"1",spare`
-	spare1        uint8 `bitfield:"1",spare`
+	IEI           uint8 // Octet 1
+	Length        uint8 // Octet 2
+	SMS_requested uint8 `bitfield:"1"`// Octet 3
+	NG_RAN_RCU    uint8 `bitfield:"1"`
+	F5GS_PNB_CIoT uint8 `bitfield:"2"`
+	EPS_PNB_CIoT  uint8 `bitfield:"2"` 
+	spare0        uint8 `bitfield:"1",reserved`
+	spare1        uint8 `bitfield:"1",reserved`
 }
+
 
 func (ie Nas5GSUpdateType) Encode(buffer *bytes.Buffer) {
 
-	return
+	
+	return 
 }
 
+
+
 func main() {
-	fmt.Println("Hello GO!")
-
-	var inquiry = new(Nas5GSUpdateType)
-
-	// Perform IOCTL on device, returns byte reader to byte response
-
-	if err := structex.Decode(byteReader, inquiry); err != nil {
+	fmt.Println("Hello GO")
+	
+	ie := Nas5GSUpdateType{ IEI:1, Length:2, EPS_PNB_CIoT:0, F5GS_PNB_CIoT:0, NG_RAN_RCU:1, SMS_requested:1, spare0: 0, spare1:0}
+		
+				
+	buff, err := structex.EncodeByteBuffer(ie)
+		
+	if err != nil {
 		panic("a problem")
-	}
+		}
+
+	fmt.Println(buff)
 
 }
